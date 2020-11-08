8f1123340c156cf9fbaac9c82c65bbdbfdf02918
chocolattips <chocolattips@gmail.com>
2020-11-08 22:03:02 +0900

    add demo 1, 2

---
+++ src/demo/sample-1.ts
@@ -0,0 +1,30 @@
+import useFSM from "..";
+
+useFSM()
+  .putSequences([
+    {
+      name: "apple",
+      onEnter(param) {
+        param.context.to("banana");
+      },
+    },
+    {
+      name: "banana",
+      onEnter(param) {
+        param.context.to("cherry");
+      },
+    },
+    {
+      name: "cherry",
+      onEnter(param) {
+        param.context.finish();
+      },
+    },
+  ])
+  .on("enter", (param) => {
+    console.log(`[ENTER] : ${param.state.name}`);
+  })
+  .on("exit", (param) => {
+    console.log(`[EXIT] : ${param.state.name}`);
+  })
+  .entry("apple");
